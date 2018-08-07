0d8bd1ec9b39dded41a9fd16d6a11b50973edd75
Nikita <mipo4t@gmail.com>
2018-08-07 17:15:08 +0400

    change defaul uotput format in bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,7 +8,7 @@ const program = commander;
 program
   .version('0.0.7')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format', 'json')
+  .option('-f, --format [type]', 'Output format', 'txt')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig, options) => {
     const type = options.format;
