6d17bc4ab75317ceb2ad1288ab183657ed02c46f
Nikita <mipo4t@gmail.com>
2018-08-07 23:03:37 +0400

    refactor bin and tests for use new lib interface

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,8 @@
+import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
-const fs = require('fs');
-const path = require('path');
-
 const resultPath = path.resolve(__dirname, '__fixtures__/result.txt');
-const result = fs.readFileSync(resultPath, 'utf8');
 
 const firstJsonPath = path.resolve(__dirname, '__fixtures__/before.json');
 const secondJsonPath = path.resolve(__dirname, '__fixtures__/after.json');
@@ -16,13 +14,22 @@ const firstIniPath = path.resolve(__dirname, '__fixtures__/before.ini');
 const secondIniPath = path.resolve(__dirname, '__fixtures__/after.ini');
 
 test('Get diff two json files', () => {
-  expect(genDiff(firstJsonPath, secondJsonPath)).toBe(result);
+  const firstRawData = fs.readFileSync(firstJsonPath, 'utf8');
+  const secondRawData = fs.readFileSync(secondJsonPath, 'utf8');
+  const result = fs.readFileSync(resultPath, 'utf8');
+  expect(genDiff(firstRawData, secondRawData, 'json')).toBe(result);
 });
 
 test('Get diff two yaml files', () => {
-  expect(genDiff(firstYamlPath, secondYamlPath)).toBe(result);
+  const firstRawData = fs.readFileSync(firstYamlPath, 'utf8');
+  const secondRawData = fs.readFileSync(secondYamlPath, 'utf8');
+  const result = fs.readFileSync(resultPath, 'utf8');
+  expect(genDiff(firstRawData, secondRawData, 'yml')).toBe(result);
 });
 
 test('Get diff two ini files', () => {
-  expect(genDiff(firstIniPath, secondIniPath)).toBe(result);
+  const firstRawData = fs.readFileSync(firstIniPath, 'utf8');
+  const secondRawData = fs.readFileSync(secondIniPath, 'utf8');
+  const result = fs.readFileSync(resultPath, 'utf8');
+  expect(genDiff(firstRawData, secondRawData, 'ini')).toBe(result);
 });
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,7 +1,37 @@
 #!/usr/bin/env node
-
+import path from 'path';
+import fs from 'fs';
 import commander from 'commander';
+
 import utility from '..';
+
+const types = new Set(['json', 'yml', 'ini']);
+
+const handler = (firstConfig, secondConfig, options) => {
+  const { format } = options;
+
+  const ext1 = path.extname(firstConfig).toLowerCase();
+  const ext2 = path.extname(secondConfig).toLowerCase();
+
+  if (ext1 !== ext2) {
+    console.log('Different types of congig files, use files of the same type');
+    return null;
+  }
+
+  const type = ext1.replace(/^\./, '');
+  if (!types.has(type)) {
+    const str = `Unsupported file type, use one of the following types: ${[...types].join(' ')}`;
+    console.log(str);
+    return null;
+  }
+
+  const data1 = fs.readFileSync(firstConfig, 'utf8');
+  const data2 = fs.readFileSync(secondConfig, 'utf8');
+
+  const result = utility(data1, data2, type, format);
+  console.log(result);
+  return true;
+};
 
 const program = commander;
 
@@ -10,10 +40,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format', 'txt')
   .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig, options) => {
-    const type = options.format;
-    console.log(utility(firstConfig, secondConfig, type));
-  })
+  .action(handler)
   .parse(process.argv);
 
 if (program.args.length === 0) {
