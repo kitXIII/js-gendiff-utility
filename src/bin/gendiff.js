c04c3ef280d9b79df6728c13b199a80cee0dfad6
Nikita <mipo4t@gmail.com>
2018-08-07 16:52:55 +0400

    prepublish change package version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import utility from '..';
 const program = commander;
 
 program
-  .version('0.0.6')
+  .version('0.0.7')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format', 'json')
   .arguments('<firstConfig> <secondConfig>')
