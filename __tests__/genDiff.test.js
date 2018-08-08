a09be2a5c89ebc66b7914b1d8f05f50750b74b1a
Nikita <mipo4t@gmail.com>
2018-08-08 12:16:27 +0400

    fix tests for refactor code by remark

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -13,26 +13,24 @@ const firstIniPath = path.resolve(__dirname, '__fixtures__/before.ini');
 const secondIniPath = path.resolve(__dirname, '__fixtures__/after.ini');
 
 test('Get diff two json files', () => {
-  const firstRawData = fs.readFileSync(firstJsonPath, 'utf8');
-  const secondRawData = fs.readFileSync(secondJsonPath, 'utf8');
   const result = fs.readFileSync(resultPath, 'utf8');
-  expect(genDiff(firstRawData, secondRawData, 'json')).toBe(result);
+  expect(genDiff(firstJsonPath, secondJsonPath)).toBe(result);
 });
 
 test('Get diff two yaml files', () => {
-  const firstRawData = fs.readFileSync(firstYamlPath, 'utf8');
-  const secondRawData = fs.readFileSync(secondYamlPath, 'utf8');
   const result = fs.readFileSync(resultPath, 'utf8');
-  expect(genDiff(firstRawData, secondRawData, 'yaml')).toBe(result);
+  expect(genDiff(firstYamlPath, secondYamlPath)).toBe(result);
 });
 
 test('Get diff two ini files', () => {
-  const firstRawData = fs.readFileSync(firstIniPath, 'utf8');
-  const secondRawData = fs.readFileSync(secondIniPath, 'utf8');
   const result = fs.readFileSync(resultPath, 'utf8');
-  expect(genDiff(firstRawData, secondRawData, 'ini')).toBe(result);
+  expect(genDiff(firstIniPath, secondIniPath, 'ini')).toBe(result);
 });
 
-test('Check wrong data type', () => {
-  expect(genDiff(null, null, '')).toMatch(/^Unsupported data type, use one of the following types/);
+test('Check wrong file type', () => {
+  expect(genDiff('file1.wrong', 'file2.wrong')).toMatch(/^Unsupported data type, use one of the following types/);
+});
+
+test('Check different files extensions', () => {
+  expect(genDiff('file1.ini', 'file2.json')).toMatch(/^Different types of congig files, use files of the same type/);
 });
