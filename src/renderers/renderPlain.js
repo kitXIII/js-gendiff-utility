c64b2c2cc2af1d93fc831475c509d95531b64e41
Nikita <mipo4t@gmail.com>
2018-08-11 18:09:23 +0400

    fix output plain bugs

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,16 +1,16 @@
 const valueStr = val => (val instanceof Object ? '[complex value]' : `${typeof val === 'string' ? `'${val}'` : `${val}`}`);
-const keyStr = (nodeParentStr, key) => `Property '${nodeParentStr === '' ? `${key}` : `${nodeParentStr}.${key}`}'`;
+const keyStr = (parents, key) => `Property '${[...parents, key].join('.')}'`;
 
-const render = (nodes, parentStr = '') => {
+const render = (nodes, parents = []) => {
   const renderers = {
-    added: (node, nodeParentStr) => `${keyStr(nodeParentStr, node.key)} was added with value ${valueStr(node.value)}`,
-    removed: (node, nodeParentStr) => `${keyStr(nodeParentStr, node.key)} was removed`,
-    changed: (node, nodeParentStr) => `${keyStr(nodeParentStr, node.key)} was updated. From ${valueStr(node.oldValue)} to ${valueStr(node.newValue)}`,
-    internal: node => render(node.children, `${node.key}`),
+    added: (node, nodeParents) => `${keyStr(nodeParents, node.key)} was added with value ${valueStr(node.value)}`,
+    removed: (node, nodeParents) => `${keyStr(nodeParents, node.key)} was removed`,
+    changed: (node, nodeParents) => `${keyStr(nodeParents, node.key)} was updated. From ${valueStr(node.oldValue)} to ${valueStr(node.newValue)}`,
+    internal: (node, nodeParents) => render(node.children, [...nodeParents, node.key]),
   };
 
   return nodes.filter(node => node.type !== 'unchanged')
-    .map(node => renderers[node.type](node, `${parentStr}`)).join('\n');
+    .map(node => renderers[node.type](node, parents)).join('\n');
 };
 
 export default render;
