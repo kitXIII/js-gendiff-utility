3fa7b4d6e7c459a2f75900abd8dfabfc626debde
Nikita <mipo4t@gmail.com>
2018-08-11 11:06:24 +0400

    back to begin fix bugs

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -3,26 +3,26 @@ import _ from 'lodash';
 const buildDiff = (object1, object2) => {
   const genDiffByKey = (key, obj1, obj2) => {
     if (!_.has(obj1, key)) {
-      return { status: 'added', key, value: obj2[key] };
+      return { type: 'added', key, value: obj2[key] };
     }
 
     if (!_.has(obj2, key)) {
-      return { status: 'removed', key, value: obj1[key] };
+      return { type: 'removed', key, value: obj1[key] };
     }
 
     if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
       const diff = buildDiff(obj1[key], obj2[key]);
-      return { status: 'internal', key, children: diff };
+      return { type: 'internal', key, children: diff };
     }
 
     if (obj1[key] === obj2[key]) {
-      return { status: 'unchanged', key, value: obj1[key] };
+      return { type: 'unchanged', key, value: obj1[key] };
     }
 
     return {
-      status: 'changed',
+      type: 'changed',
       key,
-      value: obj2[key],
+      newValue: obj2[key],
       oldValue: obj1[key],
     };
   };
--- src/renderDiff.js
+++ src/renderDiff.js
@@ -2,36 +2,25 @@
 import stringify from './utilities/stringify';
 
 const space = '  ';
-// const indentStr = depth => space.repeat(depth * 2 + 1);
 
 const getItemStr = (prefix, key, value, depth) => {
   const indentStr = depth === 0 ? '' : space.repeat(depth * 2 - 1);
   return `${indentStr}${prefix} ${key}: ${stringify(value, depth, space)}`;
 };
 
-const renders = [
-  {
-    added: (node, depth) => getItemStr('+', node.key, node.value, depth),
+const renders = {
+  added: (node, depth) => getItemStr('+', node.key, node.value, depth),
+  removed: (node, depth) => getItemStr('-', node.key, node.value, depth),
+  unchanged: (node, depth) => getItemStr(' ', node.key, node.value, depth),
+  changed: (node, depth) => `${getItemStr('-', node.key, node.oldValue, depth)}\n${getItemStr('+', node.key, node.newValue, depth)}`,
+  internal: (node, depth) => {
+    const indentStr = space.repeat(depth * 2);
+    const str = node.children.map(child => render(child, depth + 1)).join('\n'); // eslint-disable-line
+    const keyStr = node.key ? `${indentStr}${node.key}: ` : '';
+    return `${keyStr}{\n${str}\n${indentStr}}`;
   },
-  {
-    removed: (node, depth) => getItemStr('-', node.key, node.value, depth),
-  },
-  {
-    unchanged: (node, depth) => getItemStr(' ', node.key, node.value, depth),
-  },
-  {
-    changed: (node, depth) => `${getItemStr('-', node.key, node.oldValue, depth)}\n${getItemStr('+', node.key, node.newValue, depth)}`,
-  },
-  {
-    internal: (node, depth) => {
-      const indentStr = space.repeat(depth * 2);
-      const str = node.children.map(child => renders[child.type](child, depth + 1)).join('\n');
-      const keyStr = this.key ? `${indentStr}${this.key}: ` : '';
-      return `${keyStr}{\n${str}\n${indentStr}}`;
-    },
-  },
-];
+};
 
-const render = ast => renders.internal({ key: null, children: ast }, 0);
+const render = (node, depth = 0) => renders[node.type](node, depth);
 
-export default render;
+export default ast => render({ key: null, type: 'internal', children: ast }, 0);
